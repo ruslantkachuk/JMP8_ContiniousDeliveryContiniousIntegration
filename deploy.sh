36e925b4dfbd34e2d9e69f8bcd768773f788d585
ruslantkachuk <Ruslan_Tkachuk@epam.com>
2017-05-28 21:12:36 +0300

    JMP-0007 Update deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 fuser -n tcp -k 8090 > redirection &
 chmod 777 $WORKSPACE/build/libs/jmp.war
-nohup nice java -jar $WORKSPACE/build/libs/jmp.war --spring.config.location=$WORKSPACE/src/main/resources/application.yml $> $WORKSPACE/jmp_deploy/initServer.log 2>&1 &
+nohup nice java -jar $WORKSPACE/build/libs/jmp.war --spring.config.location=$WORKSPACE/src/main/resources/application.yml $> $WORKSPACE/initServer.log 2>&1 &
 
 function watch(){
 
-    tail -f $WORKSPACE/jmp_deploy/initServer.log |
+    tail -f $WORKSPACE/initServer.log |
 
         while IFS= read line
             do
