a268d4946c693219055693efba1d7e092486008d
ruslantkachuk <Ruslan_Tkachuk@epam.com>
2017-05-28 21:17:20 +0300

    JMP-0008 Update deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 fuser -n tcp -k 8090 > redirection &
+sh $WORKSPACE/gradlew clean build
 chmod 777 $WORKSPACE/build/libs/jmp.war
 nohup nice java -jar $WORKSPACE/build/libs/jmp.war --spring.config.location=$WORKSPACE/src/main/resources/application.yml $> $WORKSPACE/initServer.log 2>&1 &
 
